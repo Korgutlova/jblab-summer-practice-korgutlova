1ebeec9a46ff14a9b328a655b52fae25c22956e7
Natalia <korgutlova97@yandex.ru>
2016-08-17 02:06:41 +0300

    [SP16-5] Task added. Day1.

---
+++ Day1/src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        int radius = 3;
+        double area = 4 * Math.PI * radius * radius;
+        double volume = 4  * Math.PI * radius * radius * radius / 3;
+        System.out.println("Area of a sphere: " + area);
+        System.out.println("Volume of a sphere: " + volume);
+    }
+}
