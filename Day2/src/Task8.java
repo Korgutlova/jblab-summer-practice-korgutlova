08894cbac9265380e18af5ca0798e9efdd523719
Natalia <korgutlova97@yandex.ru>
2016-08-18 13:05:47 +0300

    [SP16-23] Tasks corrected(2).Day2

--- Day2/src/Task8.java
+++ Day2/src/Task8.java
@@ -1,16 +1,26 @@
+import java.util.Arrays;
+
 public class Task8 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int [] arrOne = new int[n];
-        int [] arrTwo = new int[n];
-        for (int i = 0; i < n; i++) {
-            arrOne[i] = Integer.parseInt(args[i + 1]);
-            arrTwo[i] = Integer.parseInt(args[i + n + 1]);
-            System.out.println(arrOne[i] + " " + arrTwo[i]);
+        int m = Integer.parseInt(args[1]);
+        int[] arrOne = new int[n];
+        int[] arrTwo = new int[m];
+        for (int i = 0; i < args.length - 2; i++) {
+            if (i < n) {
+                arrOne[i] = Integer.parseInt(args[i + 2]);
+            } else {
+                arrTwo[i - n] = Integer.parseInt(args[i + 2]);
+            }
         }
-        for (int i = 0; i < n; i++) {
-            arrOne[i] = arrOne[i] + arrTwo[i];
-            System.out.print(arrOne[i] + " ");
+        for (int i = 0; i < Math.min(n, m); i++) {
+            if (m > n) {
+                arrTwo[i] = arrTwo[i] + arrOne[i];
+            } else {
+                arrOne[i] = arrOne[i] + arrTwo[i];
+            }
         }
+        System.out.println(Arrays.toString(arrOne));
+        System.out.println(Arrays.toString(arrTwo));
     }
 }
