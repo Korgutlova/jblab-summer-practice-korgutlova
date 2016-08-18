49981cc597e9d9efe93e0957b42e6a735e77f3bb
Natalia <korgutlova97@yandex.ru>
2016-08-18 10:23:03 +0300

    [SP16-23] Tasks corrected.Day2

--- Day2/src/Task8.java
+++ Day2/src/Task8.java
@@ -1,13 +1,14 @@
 public class Task8 {
     public static void main(String[] args) {
-        int[] arrOne = new int[args[0].length()];
-        int[] arrTwo = new int[args[0].length()];
-        for (int i = 0; i < args[0].length(); i++) {
-            arrOne[i] = Character.getNumericValue(args[0].charAt(i));
-            arrTwo[i] = Character.getNumericValue(args[1].charAt(i));
+        int n = Integer.parseInt(args[0]);
+        int [] arrOne = new int[n];
+        int [] arrTwo = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrOne[i] = Integer.parseInt(args[i + 1]);
+            arrTwo[i] = Integer.parseInt(args[i + n + 1]);
             System.out.println(arrOne[i] + " " + arrTwo[i]);
         }
-        for (int i = 0; i < args[0].length(); i++) {
+        for (int i = 0; i < n; i++) {
             arrOne[i] = arrOne[i] + arrTwo[i];
             System.out.print(arrOne[i] + " ");
         }
